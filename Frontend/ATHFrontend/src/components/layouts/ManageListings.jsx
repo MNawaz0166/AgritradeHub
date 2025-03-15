9571603069006098af8520002ec1639f888eddb8
MNawaz0166 <158293844+MNawaz0166@users.noreply.github.com>
2025-03-15 23:30:23 +0500

    Update ManageListings.jsx

--- Frontend/ATHFrontend/src/components/layouts/ManageListings.jsx
+++ Frontend/ATHFrontend/src/components/layouts/ManageListings.jsx
@@ -10,7 +10,7 @@ const ManageListings = () => {
     // Fetch all listings from the backend
     const fetchListings = async () => {
       try {
-        const response = await fetch('agritrade-hub-backend1.vercel.app', {
+        const response = await fetch('https://agritrade-hub-backend.vercel.app/', {
           method: 'GET',
           headers: {
             'Authorization': `Bearer ${token}`,
