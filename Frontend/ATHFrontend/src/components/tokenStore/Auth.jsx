c87ddedf8bea4ce055ae247929a7a89255e9072c
MNawaz0166 <158293844+MNawaz0166@users.noreply.github.com>
2025-03-15 23:24:31 +0500

    Update Auth.jsx

--- Frontend/ATHFrontend/src/components/tokenStore/Auth.jsx
+++ Frontend/ATHFrontend/src/components/tokenStore/Auth.jsx
@@ -18,7 +18,7 @@ export const AuthProvider=({children})=>{
     const [user,setUser]=useState({})
     const userData= async()=>{
       try {
-        const response=await fetch("agritrade-hub-backend1.vercel.app",{
+        const response=await fetch("https://agritrade-hub-backend.vercel.app/",{
                  method:"GET",
                  headers:{
                   "Authorization":`Bearer ${token}`
